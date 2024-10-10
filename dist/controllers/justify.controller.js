94028f9c1be282a7a27d5270477febbb17016006
Sloan Gauthier <sloangauthier@gmail.com>
2024-10-10 12:14:56 +0200

    Add validation to ensure text is present in request body

--- dist/controllers/justify.controller.js
+++ dist/controllers/justify.controller.js
@@ -4,6 +4,10 @@ exports.justifyPlainText = void 0;
 const justifyText_1 = require("../utils/justifyText");
 const justifyPlainText = (req, res) => {
     const text = req.body;
+    if (!text) {
+        res.status(400).json({ message: "Text content is required" });
+        return;
+    }
     const textJustified = (0, justifyText_1.justifyText)(text);
     res.setHeader("Content-Type", "text/plain");
     res.send(textJustified);
--- src/controllers/justify.controller.ts
+++ src/controllers/justify.controller.ts
@@ -3,6 +3,11 @@ import { justifyText } from "../utils/justifyText";
 
 export const justifyPlainText = (req: Request, res: Response) => {
   const text = req.body;
+
+  if (!text) {
+    res.status(400).json({ message: "Text content is required" });
+    return;
+  }
   const textJustified = justifyText(text);
 
   res.setHeader("Content-Type", "text/plain");
